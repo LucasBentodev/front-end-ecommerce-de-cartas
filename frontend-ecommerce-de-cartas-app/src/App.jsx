a9fb9c94bb60c29041a88d0a56c5ae66119507b3
unknown <lucasbentodev@gmail.com>
2024-08-18 18:56:06 -0300

    criaçao routes pagina alterar senha e inativar cliente

--- frontend-ecommerce-de-cartas-app/src/App.jsx
+++ frontend-ecommerce-de-cartas-app/src/App.jsx
@@ -2,7 +2,8 @@ import {BrowserRouter as Router, Outlet, Route, Routes} from 'react-router-dom';
 import Header from "./components/Header.jsx";
 import AlterarCliente from './pages/AlterarCliente.jsx';
 import CriaCliente from "./pages/CriaCliente.jsx";
-
+import InativarCliente from './pages/InativarCliente.jsx';
+import AlterarSenha from './pages/AlterarSenha.jsx';
 function App() {
   return (
     <Router>
@@ -11,6 +12,8 @@ function App() {
           <Route index element={<CriaCliente />} />
           <Route path="/criacliente" element={<CriaCliente />} />
           <Route path="/alterarcliente" element={<AlterarCliente />} />
+          <Route path="/inativarCliente" element={<InativarCliente/>}/>
+          <Route path="/alterarSenha" element={<AlterarSenha/>}/>
         </Route>
       </Routes>
     </Router>
