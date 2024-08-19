f6faf98fdef90d4f594b78b9c319ce2a040c5e4f
Kaue Oliveira <kauebenk@gmail.com>
2024-08-19 18:03:53 -0300

    feature - implementa exclui cliente

--- frontend-ecommerce-de-cartas-app/src/pages/ListaClientes.jsx
+++ frontend-ecommerce-de-cartas-app/src/pages/ListaClientes.jsx
@@ -1,9 +1,13 @@
+import * as React from "react";
 import {useContext} from "react";
 import {ClienteContext} from "../contexts/ClienteContext.tsx";
 import {Typography} from "@mui/material";
 import {DataGrid} from "@mui/x-data-grid";
-import MenuTresPontos from "../components/MenuTresPontos.jsx";
 import {useTheme} from "@mui/material/styles";
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 const columns = [
   { field: 'id', headerName: 'ID', flex: 0.2 },
@@ -13,8 +17,8 @@ const columns = [
     field: 'actions',
     headerName: 'Opções',
     width: 150,
-    renderCell: () => (
-      <MenuTresPontos/>
+    renderCell: (row) => (
+      <MenuTresPontos id={row.id} />
     ),
   },
 ];
@@ -44,3 +48,69 @@ function ListaClientes() {
 }
 
 export default ListaClientes;
+
+const options = [
+  'Editar',
+  'Excluir',
+];
+
+const ITEM_HEIGHT = 48;
+
+function MenuTresPontos({id}) {
+  const {removeCliente} = useContext(ClienteContext);
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleEdit = () => {
+    alert("Editado");
+    handleClose();
+  }
+  const handleDelete = () => {
+    removeCliente(id);
+    handleClose();
+  }
+
+  return (
+    <div>
+      <IconButton
+        aria-label="more"
+        id="long-button"
+        aria-controls={open ? 'long-menu' : undefined}
+        aria-expanded={open ? 'true' : undefined}
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <MoreVertIcon/>
+      </IconButton>
+      <Menu
+        id="long-menu"
+        MenuListProps={{
+          'aria-labelledby': 'long-button',
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          style: {
+            maxHeight: ITEM_HEIGHT * 4.5,
+            width: '20ch',
+          },
+        }}
+      >
+        <MenuItem key={"Editar"} selected={"Editar" === 'Pyxis'} onClick={handleEdit}>
+          {"Editar"}
+        </MenuItem>
+        <MenuItem key={"Excluir"} selected={"Excluir" === 'Pyxis'} onClick={handleDelete}>
+          {"Excluir"}
+        </MenuItem>
+    </Menu>
+</div>
+)
+  ;
+}
